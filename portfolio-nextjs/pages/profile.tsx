85037fe1954cd66811618b620679e738f1efbf9a
danilobatson <djbatson19@gmail.com>
2022-12-01 15:21:58 -0800

    loads image and header

--- portfolio-nextjs/pages/profile.tsx
+++ portfolio-nextjs/pages/profile.tsx
@@ -90,7 +90,68 @@ export default function ProfilePage() {
             src={require('assets/img/path4.png')}
           />
           <Container className='align-items-center'>
-            <Row>
+            <div className='content-center'>
+              <Row className='row-grid justify-content-between align-items-center text-left'>
+                <Col lg='6' md='6'>
+                  <h1 className='text-white'>Danilo Batson</h1>
+                  <p className='text-white mb-3'>
+                    A wonderful serenity has taken possession of my entire soul,
+                    like these sweet mornings of spring which I enjoy with my
+                    whole heart. I am alone, and feel...
+                  </p>
+                  <div className='btn-wrapper mb-3'>
+                    <p className='category text-success d-inline'>
+                      From 9.99%/mo
+                    </p>
+                    <Button
+                      className='btn-link'
+                      color='success'
+                      href='#pablo'
+                      onClick={(e) => e.preventDefault()}
+                      size='sm'
+                    >
+                      <i className='tim-icons icon-minimal-right' />
+                    </Button>
+                  </div>
+                  <div className='btn-wrapper'>
+                    <div className='button-container'>
+                      <Button
+                        className='btn-icon btn-simple btn-round btn-neutral'
+                        color='default'
+                        href='#pablo'
+                        onClick={(e) => e.preventDefault()}
+                      >
+                        <i className='fab fa-twitter' />
+                      </Button>
+                      <Button
+                        className='btn-icon btn-simple btn-round btn-neutral'
+                        color='default'
+                        href='#pablo'
+                        onClick={(e) => e.preventDefault()}
+                      >
+                        <i className='fab fa-dribbble' />
+                      </Button>
+                      <Button
+                        className='btn-icon btn-simple btn-round btn-neutral'
+                        color='default'
+                        href='#pablo'
+                        onClick={(e) => e.preventDefault()}
+                      >
+                        <i className='fab fa-facebook' />
+                      </Button>
+                    </div>
+                  </div>
+                </Col>
+                <Col lg='4' md='5'>
+                  <Image
+                    alt='...'
+                    className='img-fluid'
+                    src={require('assets/img/Danilo.jpeg')}
+                  />
+                </Col>
+              </Row>
+            </div>
+            {/* <Row>
               <Col lg='6' md='6'>
                 <h1 className='profile-title text-left'>Mike Scheinder</h1>
                 <h5 className='text-on-back'>01</h5>
@@ -284,7 +345,7 @@ export default function ProfilePage() {
                   </CardBody>
                 </Card>
               </Col>
-            </Row>
+            </Row> */}
           </Container>
         </div>
         <div className='section'>
