f26861f895cdca618a113f7208a0b97ea5c51e98
danilobatson <djbatson19@gmail.com>
2022-12-01 12:32:48 -0800

    adds next js

---
+++ portfolio-nextjs/components/index.tsx
@@ -0,0 +1,7 @@
+import Head from 'next/head';
+import Image from 'next/image';
+import styles from '../styles/Home.module.css';
+
+export default function App(): JSX.Element {
+  return <div className={styles.container}>Test</div>;
+}
