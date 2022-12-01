c3c04505ed63546c9b1b79be535c3f063b7744e2
danilobatson <djbatson19@gmail.com>
2022-12-01 15:00:23 -0800

    sets up design system

--- portfolio-nextjs/pages/_app.tsx
+++ portfolio-nextjs/pages/_app.tsx
@@ -1,6 +1,9 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
 import 'bootstrap/dist/css/bootstrap.css';
+import '../assets/css/nucleo-icons.css';
+import '../assets/scss/blk-design-system-react.scss';
+import '../assets/demo/demo.css';
 
 export default function App({ Component, pageProps }: AppProps) {
   return <Component {...pageProps} />
